0ef674943471efb29c3bac8b3e38673a79b40a6e
Harshit Ruwali <harshitruwali@outlook.com>
2022-07-12 15:32:44 +0530

    solved ownership questions

---
+++ 5.1-ownership/1.rs
@@ -0,0 +1,15 @@
+
+fn main() {
+    // Use as many approaches as you can to make it work
+    let x = String::from("hello, world");
+    let y = x.clone();
+    println!("{},{}",x,y);
+}
+
+
+fn main() {
+    // Use as many approaches as you can to make it work
+    let x = "hello, world";
+    let y = x;
+    println!("{},{}",x,y);
+}
